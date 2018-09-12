fbd9244ce8ba3d286eec7c5793fa9092cfc330d0
Fariz <Fariz@LAPTOP-JQ2AQ0I3>
2018-09-12 13:58:46 +0700

    Pembuatan main, initialisasi, dan penginputan data

---
+++ src/tugas/Tugas.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas;
+import java.util.Scanner;
+/**
+ *
+ * @author Fariz
+ */
+public class Tugas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String namaKasir, namaBarang, hargaBarang, codeBarang, diskon;
+        Scanner txt = new Scanner(System.in);
+        System.out.println("Fariz Akbar Ade Rian / XRPL6 / 23");
+        System.out.println("Selamat datang di toko");
+        System.out.println(" ");
+        System.out.println("Masukan nama kasir");
+        namaKasir=txt.nextLine();
+        System.out.println(" ");
+        System.out.println("Masukan nama barang");
+        namaBarang=txt.nextLine();
+    }
+    
+}
