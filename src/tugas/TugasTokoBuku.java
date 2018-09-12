1aad6641c6f246049df8bc4a288d6e8f283fd905
Fariz <Fariz@LAPTOP-JQ2AQ0I3>
2018-09-12 14:59:15 +0700

    Selesai

--- src/tugas/TugasTokoBuku.java
+++ src/tugas/TugasTokoBuku.java
@@ -15,45 +15,79 @@ public class TugasTokoBuku {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String namaKasir, namaBarang, codeBarang, diskon;
-        int hargaBarang;
+        String namaKasir, namaBarang, codeBarang, ulang=" ";
+        int hargaBarang, jmlh, diskon=0;
         Scanner txt = new Scanner(System.in);
         System.out.println("Fariz Akbar Ade Rian / XRPL6 / 23");
         System.out.println("Selamat datang di toko");
+        do{
         System.out.println(" ");
         System.out.println("Masukan nama kasir");
         namaKasir=txt.nextLine();
-        System.out.println(" ");
+        System.out.println(" ");        
         System.out.println("Masukan nama barang");
         namaBarang=txt.nextLine();
         System.out.println("Masukan kode barang");
         codeBarang=txt.nextLine();
-        System.out.print("Masukan harga barang \nRp.");
+        System.out.print("Masukan harga barang satuan \nRp.");
         hargaBarang=txt.nextInt();
+        System.out.println("Masukan jumlah barang");
+        jmlh=txt.nextInt();
+        hargaBarang*=jmlh;
+        if(hargaBarang>=100000){
+            if(hargaBarang>=200000){
+                if(hargaBarang>=500000){
+                    diskon=50;
+                }else{diskon=20;   
+                }
+            }else{diskon=10;
+            }
+        }
+        System.out.println("Total harga Adalah Rp."+hargaBarang);
         System.out.println("\\");
         System.out.println("\\");
-        hitung(hargaBarang);
-    }
+        int hargaAkhir=hitung(hargaBarang, diskon);
+        tampilAkhir(namaKasir, namaBarang, codeBarang, hargaAkhir, jmlh, diskon);
+        System.out.println("Ingin Transaksi lagi??[ya/tidak]");
+        ulang=txt.next();
+        }while(ulang.equalsIgnoreCase("ya"));
+        if(ulang.equalsIgnoreCase("tidak")){
+            System.out.println("Terimakasih atas kunjunganya.");
+        }
+        }
     
-    public static void hitung(int nBrg){
+    public static int hitung(int nBrg,int diskon){
         int harga=nBrg,hargaAkhir=0,harga2,harga3;
         if(harga>=100000){
             if(harga>=200000){
                 if(harga>=500000){
                     harga2=harga/100;
-                    harga3=harga2*50;
+                    harga3=harga2*diskon;
                     hargaAkhir=harga-harga3;
                 }else{
                  harga2=harga/100;
-                 harga3=harga2*20;
+                 harga3=harga2*diskon;
                  hargaAkhir=harga-harga3;   
                 }
             }else{
                 harga2=harga/100;
-                harga3=harga2*10;
+                harga3=harga2*diskon;
                 hargaAkhir=harga-harga3;
             }
         }
         System.out.println("Harga yang harus dibayar adalah Rp."+hargaAkhir);
-    } 
+        return hargaAkhir;
+    }
+    public static void tampilAkhir(String nama,String barang, String kode, int harga,int jmlh,int diskon){
+        System.out.println("    --------    ");
+        System.out.println("        |       ");
+        System.out.println("        |       ");
+        System.out.println("    --------    ");
+        System.out.println("Nama Kasir \t: "+ nama);
+        System.out.println("Nama Barang \t: "+ barang);
+        System.out.println("Jumlah Barang \t: "+ jmlh);
+        System.out.println("Diskon \t\t: "+diskon+"%");
+        System.out.println("Kode Barang \t: "+ kode);
+        System.out.println("Harga Total \t: Rp."+ harga);
+    }
 }
